f9d1ad4fd423fd47dc8dc4557a8b21979dec7ec2
sheelarajeshkumar <gangstarsrk@gmail.com>
2017-06-25 15:25:19 +0530

    Code repo for project

---
+++ app/src/main/java/thehub/life/fd/SplashScreen.java
@@ -0,0 +1,37 @@
+package thehub.life.fd;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+
+        getSupportActionBar().hide();
+        //Thread for splashScreen
+       new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(3000);
+                    Intent it = new Intent(getApplicationContext(), signup.class);
+                    startActivity(it);
+                    finish();
+                }catch (Exception e){
+                    Log.e("Thread Splash Screen",e.getMessage());
+                }
+            }
+        }.start();
+
+
+
+
+    }
+}

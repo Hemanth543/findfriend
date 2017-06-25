98d2ca0da9a41daa88dbc0a141e5196d163b70f0
sheelarajeshkumar <gangstarsrk@gmail.com>
2017-06-25 21:34:37 +0530

    updated

--- app/src/main/java/thehub/life/fd/MapsActivity.java
+++ app/src/main/java/thehub/life/fd/MapsActivity.java
@@ -39,8 +39,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng position1 = new LatLng(Double.parseDouble(String.valueOf(17.385044)),Double.parseDouble(String.valueOf(78.486671)));
+        mMap.addMarker(new MarkerOptions().position(position1).title("THE HUB"));
+        float zoomLevel = 18.0f;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position1, zoomLevel));
     }
 }
